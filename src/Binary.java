9d619dcca79e0e34d049cb7a67be63f6b84eaa78
chuchumj <janeibodje@gmail.com>
2020-01-12 08:56:21 -0600

    I added some new files

--- src/Binary.java
+++ src/Binary.java
@@ -1,6 +1,9 @@
 class Binary implements BinaryOperator{
     public int yield(int a, int b){
         int z = a + b;
+		System.out.println(z);
+		System.out.println(a);
+		
 
         return z;
     }
